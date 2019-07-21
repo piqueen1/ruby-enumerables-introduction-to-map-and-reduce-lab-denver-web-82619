9dfa47ea3449e6aaa0277bc1ae0156087baaae24
Finn Thye <piqueen@gmail.com>
2019-07-21 18:08:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,6 +51,10 @@ def reduce_to_total(source_array, *starting_point)
   i = 0
   total = 0
   
+  if starting_point do
+    total = starting_point
+  end
+  
   while i < source_array.length do
     total += source_array[i]
     i += 1
