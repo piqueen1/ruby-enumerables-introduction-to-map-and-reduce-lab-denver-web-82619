52d10b18400d7b05b537c2d3ee5ed9f953c3bd0a
Finn Thye <piqueen@gmail.com>
2019-07-21 17:57:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,6 +24,15 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    new_array.push(2*source_array[i])
+    i += 1
+  end
+  
+  new_array
 end
 
 def map_to_square(source_array)
