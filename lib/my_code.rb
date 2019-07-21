114fea95fcc3a0c41863ea7fc0e555f5d9a1adaf
Finn Thye <piqueen@gmail.com>
2019-07-21 17:56:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,6 +12,15 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    new_array.push(source_array[i])
+    i += 1
+  end
+  
+  new_array
 end
 
 def map_to_double(source_array)
