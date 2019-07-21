26e7044fc950c144dc14199470efd21af3a8cd39
Finn Thye <piqueen@gmail.com>
2019-07-21 17:37:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,21 @@
 # My Code here....
-map_to_negativize(source_array)
-map_to_no_change(source_array)
-map_to_double(source_array)
-map_to_square(source_array)
+def map_to_negativize(source_array)
+end
+
+def map_to_no_change(source_array)
+end
+
+def map_to_double(source_array)
+end
+
+def map_to_square(source_array)
+end
+  
+def reduce_to_total(source_array, starting_point)
+end
+
+def reduce_to_all_true(source_array)
+end
+
+def reduce_to_any_true(source_array)
+end
