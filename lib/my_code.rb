6a97eba6ae107c97dccd4ce35a9d9a6abedaf85a
Finn Thye <piqueen@gmail.com>
2019-07-21 17:58:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,6 +36,15 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
+    i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    new_array.push(source_array[i])
+    i += 1
+  end
+  
+  new_array
 end
   
 def reduce_to_total(source_array, starting_point)
