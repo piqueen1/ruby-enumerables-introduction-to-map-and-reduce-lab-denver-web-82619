493b8a66a042c22dce109103574bd43fed47ab2e
Finn Thye <piqueen@gmail.com>
2019-07-21 18:25:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -74,4 +74,15 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
+    i = 0
+    
+  while i < source_array.length do
+    if source_array[i] == true
+      return true
+    end
+  
+    i += 1
+  end
+  
+  return false
 end
